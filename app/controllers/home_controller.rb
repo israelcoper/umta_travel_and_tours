64bb66e0c3cf398209dbe0ee46adc27af8de9de0
Israel B. Coper <coper_israelb@yahoo.com>
2017-04-20 15:34:49 +0800

    Bootstrap - About us page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    render layout: "home"
+  end
+
+  def about_us
+    render layout: "application"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "about-us", to: "home#about_us"
   root "home#index"
 end
